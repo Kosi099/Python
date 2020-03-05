d92ff33be28cd3e4f46c54b7d8469b54fef33e06
MichKos <kosiara099@wp.pl>
2020-03-05 10:55:29 +0100

    commit initial

--- zad1.py
+++ zad1.py
@@ -1,6 +1,36 @@
+# zadanie 10
 import sys
-a = int(input("Podaj liczbe = "))
-for i in range(a+1):
-    for j in range(i):
-        sys.stdout.write("A")  
-    sys.stdout.write("\n")
+# a = int(input("Podaj liczbe = "))
+# for i in range(a+1):
+#     for j in range(i):
+#         sys.stdout.write("A")  
+#     sys.stdout.write("\n")
+
+# zadanie 11
+# b = int(input("Podaj liczbe = "))
+# for i in range(b+1):
+#     for j in range(0,b-i):
+#         sys.stdout.write(" ")
+#     for k in range(0,i+1):
+#         sys.stdout.write("*")
+#     sys.stdout.write("\n")
+
+
+c = int(input("Podaj liczbe nieparzysta = "))
+p = 1
+
+for i in range(c+1):
+    if i<=c/2:  
+        for j in range(0,c-i):
+            sys.stdout.write(" ")
+        for k in range(0,p):
+            sys.stdout.write("o")
+        p+=2
+        sys.stdout.write("\n") 
+    else:
+        for l in range(c-1,0):
+            sys.stdout.write(" ")
+        for m in range(p,0):
+            sys.stdout.write("o")
+        p-=2
+        sys.stdout.write("\n")
