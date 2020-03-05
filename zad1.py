1aee24c4c2ad8d5ef8c032df8b806e2ef469c9df
MichKos <kosiara099@wp.pl>
2020-03-05 10:19:30 +0100

    commit initial

--- zad1.py
+++ zad1.py
@@ -1,2 +1,6 @@
-a = input("Podaj liczbe = ")
-print(a)
+import sys
+a = int(input("Podaj liczbe = "))
+for i in range(a+1):
+    for j in range(i):
+        sys.stdout.write("A")  
+    sys.stdout.write("\n")
