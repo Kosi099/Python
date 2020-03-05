939b94b036cb9c471b28c8a27ca3b488f7d43c81
MichKos <kosiara099@wp.pl>
2020-03-05 11:14:25 +0100

    commit initial

--- zad1.py
+++ zad1.py
@@ -1,3 +1,12 @@
+# zadanie 1
+# print ("Ala ma kota".count(" "))
+
+# zadanie 2
+# a = int(input("Podaj liczba = "))
+# b = int(input("Podaj liczba = "))
+# c=a*b
+# print(c)
+
 # zadanie 10
 import sys
 # a = int(input("Podaj liczbe = "))
@@ -16,21 +25,21 @@ import sys
 #     sys.stdout.write("\n")
 
 
-c = int(input("Podaj liczbe nieparzysta = "))
-p = 1
+# c = int(input("Podaj liczbe nieparzysta = "))
+# p = -1
 
-for i in range(c+1):
-    if i<=c/2:  
-        for j in range(0,c-i):
-            sys.stdout.write(" ")
-        for k in range(0,p):
-            sys.stdout.write("o")
-        p+=2
-        sys.stdout.write("\n") 
-    else:
-        for l in range(c-1,0):
-            sys.stdout.write(" ")
-        for m in range(p,0):
-            sys.stdout.write("o")
-        p-=2
-        sys.stdout.write("\n")
+# for i in range(c):
+#     if i<=c/2: 
+#         p+=2 
+#         for j in range(0,c-i):
+#             sys.stdout.write(" ")
+#         for k in range(0,p):
+#             sys.stdout.write("o")
+#         sys.stdout.write("\n") 
+#     else:
+#         for l in range(0,i+1):
+#             sys.stdout.write(" ")
+#         for m in range(0,p-2):
+#             sys.stdout.write("o")
+#         p-=2
+#         sys.stdout.write("\n")
